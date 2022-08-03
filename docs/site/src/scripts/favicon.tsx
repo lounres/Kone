a69703c61eb165a457adc5556acddfc20b94ed9d
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2022-08-03 14:43:37 +0300

    Try to adapt favicon to preferred theme.

---
+++ docs/site/src/scripts/favicon.tsx
@@ -0,0 +1,21 @@
+// import React, {useState} from 'react';
+//
+// const [favicon, setFavicon] = useState('violet');
+//
+// window
+//     .matchMedia('prefers-color-scheme: dark')
+//     .addEventListener("change", (event) => {
+//         setFavicon(event.matches ? 'orange' : 'violet')
+//     })
+
+
+// document.head(
+//     <link rel='icon' href={`img/logos/kone-logo-original-${favicon}.svg`}/>
+// )
+
+// export function faviconProvider(context, options) {
+//     return {
+//         name: 'docusaurus-plugin',
+//
+//     }
+// }
