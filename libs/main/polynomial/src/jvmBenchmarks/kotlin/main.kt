d3fda587ddbb791ff13d62714c64af1c0cebbeca
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2023-01-15 17:47:49 +0300

    Update copyrights. Fix copyright scope.

--- libs/main/polynomial/src/jvmBenchmarks/kotlin/main.kt
+++ libs/main/polynomial/src/jvmBenchmarks/kotlin/main.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2023 Gleb Minaev
+ * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
+ */
+
 @file:Suppress("unused")
 
 package com.lounres.kone.polynomial.benchmarks
