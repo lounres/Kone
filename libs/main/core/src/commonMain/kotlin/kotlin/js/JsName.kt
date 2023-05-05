4736b5d2d9dd566d0b9106040bae5a7805afbdf5
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2023-05-05 12:02:34 +0300

    Introduce a stub for `JsName` annotation.

---
+++ libs/main/core/src/commonMain/kotlin/kotlin/js/JsName.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2023 Gleb Minaev
+ * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
+ */
+
+package kotlin.js
+import kotlin.annotation.AnnotationTarget.*
+
+@Target(CLASS, FUNCTION, PROPERTY, CONSTRUCTOR, PROPERTY_GETTER, PROPERTY_SETTER)
+public annotation class JsName(val name: String)
