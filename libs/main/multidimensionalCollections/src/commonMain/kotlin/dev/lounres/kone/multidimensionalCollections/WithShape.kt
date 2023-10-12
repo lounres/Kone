c070c7f660b5bda22cd73554cda50ace50083ec3
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2023-10-12 23:29:10 +0300

    WIP: prototyping API for multidimensional collections.

---
+++ libs/main/multidimensionalCollections/src/commonMain/kotlin/dev/lounres/kone/multidimensionalCollections/WithShape.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2023 Gleb Minaev
+ * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
+ */
+
+package dev.lounres.kone.multidimensionalCollections
+
+
+public interface WithShape {
+    public val shape: ShapeND
+}
