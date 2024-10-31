97342a001a771906dfd71db6243a0b57ea2c8139
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2024-10-31 14:11:54 +0300

    Experiment in progress.

--- libs/main/polynomial/src/jvmTest/kotlin/dev/lounres/kone/polynomial/NumberedPolynomialUtilTest.kt
+++ libs/main/polynomial/src/jvmTest/kotlin/dev/lounres/kone/polynomial/NumberedPolynomialUtilTest.kt
@@ -1,5 +1,5 @@
 ///*
-// * Copyright © 2023 Gleb Minaev
+// * Copyright © 2024 Gleb Minaev
 // * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
 // */
 //
