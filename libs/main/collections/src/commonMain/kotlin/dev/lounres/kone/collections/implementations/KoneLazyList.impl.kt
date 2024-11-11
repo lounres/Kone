a5088fd6a2189998cbe6a7c4512f4f7129a223e3
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2024-11-11 19:27:01 +0300

    Experiment in progress.

--- libs/main/collections/src/commonMain/kotlin/dev/lounres/kone/collections/implementations/KoneLazyList.impl.kt
+++ libs/main/collections/src/commonMain/kotlin/dev/lounres/kone/collections/implementations/KoneLazyList.impl.kt
@@ -15,6 +15,50 @@ import dev.lounres.kone.option.orElse
 import dev.lounres.kone.repeat
 
 
+/**
+ * Represents a settable list which elements are either set and stored in an array or
+ * marked to be initialised by the provided [generator].
+ *
+ * # Implementation details
+ *
+ * This implementation holds [size] value, [generator] function, and [buffer] array of size [size].
+ * Each time value with index `i` is accessed, either the `i`th value from buffer is used or
+ * [generator] is invoked on `i` to return resulting value.
+ * The computed by [generator] value is stored in the [buffer] the moment [generator]'s invocation is finished.
+ *
+ * That means that at the beginning the [buffer] is empty.
+ * Right after that access to any `i`th element would invoke [generator] on `i`,
+ * store its result in [buffer] and return this result.
+ * But if you at first set some value `x` to the `i`th index
+ * then the value is stored in the `i`th position in [buffer].
+ * And after that access `i`th element returns `x` without extra actions.
+ *
+ * To represent presence or absence of the `i`th value, [Some] and [None] are used respectively.
+ *
+ * ## Time complexity of operations
+ *
+ * Be aware that the formulas do not include [generator] time invocation!
+ *
+ * | Operation                                                          | Worst case    | Average       |
+ * |--------------------------------------------------------------------|---------------|---------------|
+ * | [size]                                                             | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [get]                                                              | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [set]                                                              | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator]                                                         | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iteratorFrom]                                                     | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.hasNext][KoneSettableLinearIterator.hasNext]             | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.hasPrevious][KoneSettableLinearIterator.hasPrevious]     | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.getNext][KoneSettableLinearIterator.getNext]             | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.getPrevious][KoneSettableLinearIterator.getPrevious]     | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.setNext][KoneSettableLinearIterator.setNext]             | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.setPrevious][KoneSettableLinearIterator.setPrevious]     | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.moveNext][KoneSettableLinearIterator.moveNext]           | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.movePrevious][KoneSettableLinearIterator.movePrevious]   | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.nextIndex][KoneSettableLinearIterator.nextIndex]         | \(\Theta(1)\) | \(\Theta(1)\) |
+ * | [iterator.previousIndex][KoneSettableLinearIterator.previousIndex] | \(\Theta(1)\) | \(\Theta(1)\) |
+ *
+ * @usesMathJax
+ */
 //@Serializable(with = KoneLazyListWithContextSerializer::class)
 @OptIn(DelicateCollectionsInheritanceAPI::class)
 public class KoneLazyList<Element>(
@@ -28,70 +72,85 @@ public class KoneLazyList<Element>(
         buffer[index] = Some(element)
     }
 
-    override fun iterator(): KoneSettableLinearIterator<Element> = Iterator(size = size, buffer = buffer, generator = generator)
-    override fun iteratorFrom(index: UInt): KoneSettableLinearIterator<Element> = Iterator(size = size, currentIndex = index, buffer = buffer, generator = generator)
+    override fun iterator(): KoneSettableLinearIterator<Element> =
+        Iterator(
+            size = size,
+            currentIndex = 0u,
+            buffer = buffer,
+            generator = generator,
+        )
+    override fun iteratorFrom(index: UInt): KoneSettableLinearIterator<Element> =
+        if (index > size) indexOutOfBoundsException(index, size)
+        else Iterator(
+            size = size,
+            currentIndex = index,
+            buffer = buffer,
+            generator = generator,
+        )
+    
+    // TODO: Don't know what is the best solution: access-less `hashCode` and `equals` or generating all values.
+//    override fun hashCode(): Int {
+//        var hashCode = 1
+//        for (i in 0u..<size) {
+//            hashCode = 31 * hashCode + this[i].hashCode()
+//        }
+//        return hashCode
+//    }
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (other !is KoneList<*>) return false
+//        if (this.size != other.size) return false
+//
+//        when (other) {
+//            is KoneLazyList<*> ->
+//                repeat(size) {
+//                    if (this[it] != other[it]) return false
+//                }
+//            else -> {
+//                val otherIterator = other.iterator()
+//                for (i in 0u ..< size) {
+//                    if (this[i] != otherIterator.getAndMoveNext()) return false
+//                }
+//            }
+//        }
+//
+//        return true
+//    }
 
-    override fun hashCode(): Int {
-        var hashCode = 1
-        for (i in 0u..<size) {
-            hashCode = 31 * hashCode + this[i].hashCode()
-        }
-        return hashCode
-    }
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is KoneList<*>) return false
-        if (this.size != other.size) return false
-
-        when (other) {
-            is KoneLazyList<*> ->
-                repeat(size) {
-                    if (this[it] != other[it]) return false
-                }
-            else -> {
-                val otherIterator = other.iterator()
-                for (i in 0u ..< size) {
-                    if (this[i] != otherIterator.getAndMoveNext()) return false
-                }
-            }
-        }
-
-        return true
-    }
-
-    internal class Iterator<E>(val size: UInt, var currentIndex: UInt = 0u, val buffer: KoneMutableArray<Option<E>>, val generator: (UInt) -> E): KoneSettableLinearIterator<E> {
-        init {
-            if (currentIndex > size) indexOutOfBoundsException(currentIndex, size)
-        }
-        // TODO: Move `hasX`, `moveX`, and `XIndex` methods to separate interface. They are the same as for KoneResizableArrayList.
+    internal class Iterator<Element>(
+        val size: UInt,
+        var currentIndex: UInt,
+        val buffer: KoneMutableArray<Option<Element>>,
+        val generator: (UInt) -> Element
+    ): KoneSettableLinearIterator<Element> {
         override fun hasNext(): Boolean = currentIndex < size
-        override fun getNext(): E {
-            if (!hasNext()) indexOutOfBoundsException(currentIndex, size)
+        override fun getNext(): Element {
+            if (!hasNext()) noNextElementInIteratorException()
             return buffer[currentIndex].orElse { generator(currentIndex).also { buffer[currentIndex] = Some(it) } }
         }
-        override fun setNext(element: E) {
-            if (!hasNext()) indexOutOfBoundsException(currentIndex, size)
+        override fun setNext(element: Element) {
+            if (!hasNext()) noNextElementInIteratorException()
             buffer[currentIndex] = Some(element)
         }
         override fun moveNext() {
-            if (!hasNext()) indexOutOfBoundsException(currentIndex, size)
+            if (!hasNext()) noNextElementInIteratorException()
             currentIndex++
         }
-        override fun nextIndex(): UInt = if (hasNext()) currentIndex else indexOutOfBoundsException(currentIndex, size)
+        override fun nextIndex(): UInt = if (hasNext()) currentIndex else noNextElementInIteratorException()
 
         override fun hasPrevious(): Boolean = currentIndex > 0u
-        override fun getPrevious(): E {
-            if (!hasPrevious()) indexOutOfBoundsException(currentIndex, size)
+        override fun getPrevious(): Element {
+            if (!hasPrevious()) noPreviousElementInIteratorException()
             return buffer[currentIndex - 1u].orElse { generator(currentIndex - 1u).also { buffer[currentIndex - 1u] = Some(it) } }
         }
-        override fun setPrevious(element: E) {
-            if (!hasPrevious()) indexOutOfBoundsException(currentIndex, size)
+        override fun setPrevious(element: Element) {
+            if (!hasPrevious()) noPreviousElementInIteratorException()
             buffer[currentIndex] = Some(element)
         }
         override fun movePrevious() {
-            if (!hasPrevious()) indexOutOfBoundsException(currentIndex, size)
+            if (!hasPrevious()) noPreviousElementInIteratorException()
             currentIndex--
         }
-        override fun previousIndex(): UInt = if (hasPrevious()) currentIndex - 1u else indexOutOfBoundsException(currentIndex, size)
+        override fun previousIndex(): UInt = if (hasPrevious()) currentIndex - 1u else noPreviousElementInIteratorException()
     }
 }
--- libs/main/collections/src/commonMain/kotlin/dev/lounres/kone/collections/implementations/KoneVirtualList.impl.kt
+++ libs/main/collections/src/commonMain/kotlin/dev/lounres/kone/collections/implementations/KoneVirtualList.impl.kt
@@ -18,10 +18,7 @@ import dev.lounres.kone.collections.*
  *
  * This implementation holds only [size] value and [generator] function.
  * Each time value with index `i` is accessed, [generator] is invoked on `i`
- * and the result is returned. The result is not stored anywhere
- *
- * That's why it has perfect access time complexity
- * while having bad mutability time complexity.
+ * and the result is returned. The result is not stored anywhere.
  *
  * ## Time complexity of operations
  *
@@ -57,33 +54,34 @@ public class KoneVirtualList<Element>(
         if (index > size) indexOutOfBoundsException(index, size)
         else Iterator(size = size, currentIndex = index, generator = generator)
 
-    override fun hashCode(): Int {
-        var hashCode = 1
-        for (i in 0u..<size) {
-            hashCode = 31 * hashCode + this[i].hashCode()
-        }
-        return hashCode
-    }
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is KoneList<*>) return false
-        if (this.size != other.size) return false
-
-        when (other) {
-            is KoneVirtualList<*> ->
-                for (i in 0u..<size) {
-                    if (this[i] != other[i]) return false
-                }
-            else -> {
-                val otherIterator = other.iterator()
-                for (i in 0u ..< size) {
-                    if (this[i] != otherIterator.getAndMoveNext()) return false
-                }
-            }
-        }
-
-        return true
-    }
+    // TODO: Don't know what is the best solution: access-less `hashCode` and `equals` or generating all values.
+//    override fun hashCode(): Int {
+//        var hashCode = 1
+//        for (i in 0u..<size) {
+//            hashCode = 31 * hashCode + this[i].hashCode()
+//        }
+//        return hashCode
+//    }
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (other !is KoneList<*>) return false
+//        if (this.size != other.size) return false
+//
+//        when (other) {
+//            is KoneVirtualList<*> ->
+//                for (i in 0u..<size) {
+//                    if (this[i] != other[i]) return false
+//                }
+//            else -> {
+//                val otherIterator = other.iterator()
+//                for (i in 0u ..< size) {
+//                    if (this[i] != otherIterator.getAndMoveNext()) return false
+//                }
+//            }
+//        }
+//
+//        return true
+//    }
 
     internal class Iterator<Element>(val size: UInt, var currentIndex: UInt = 0u, val generator: (UInt) -> Element): KoneLinearIterator<Element> {
         init {
