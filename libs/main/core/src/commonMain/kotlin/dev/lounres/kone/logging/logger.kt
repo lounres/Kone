c86b1b1002c2d1b76345abeee82a6b158e269197
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-01-17 18:18:01 +0300

    Experiment in progress.

--- libs/main/core/src/commonMain/kotlin/dev/lounres/kone/logging/logger.kt
+++ libs/main/core/src/commonMain/kotlin/dev/lounres/kone/logging/logger.kt
@@ -7,9 +7,10 @@ package dev.lounres.kone.logging
 
 import dev.lounres.logKube.core.CurrentPlatformLogger
 import dev.lounres.logKube.core.LogAcceptor
+import dev.lounres.logKube.core.LogLevel
 
 
 /**
  * Kone-wide logger. It's a source of all Kone's logs. You can add your own [LogAcceptor] to accept its logs.
  */
-public val koneLogger: CurrentPlatformLogger = CurrentPlatformLogger(name = "Kone core logger")
+public val koneLogger: CurrentPlatformLogger<LogLevel> = CurrentPlatformLogger(name = "Kone core logger")
