410f68d0167b23f2d0b5f43424a73bef65bd8144
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-03-18 23:19:18 +0300

    Experiment in progress.

--- libs/main/numberTheory/src/commonMain/kotlin/dev/lounres/kone/algebraic/BigLongRational.kt
+++ libs/main/numberTheory/src/commonMain/kotlin/dev/lounres/kone/algebraic/BigLongRational.kt
@@ -64,7 +64,7 @@ internal value class UBigLongBigLongQuotientsByGCD(val first: UBigLong, val seco
 }
 
 internal fun divideByGCD(first: UBigLong, second: BigLong): UBigLongBigLongQuotientsByGCD = context(UBigLong.context, BigLong.context) {
-    val gcd = gcd(first, second.absoluteValue)
+    val gcd = context(UBigLong.context) /* FIXME: KT-75150 */ { gcd(first, second.absoluteValue) }
     
     if (gcd.isZero()) UBigLongBigLongQuotientsByGCD(UBigLong.context.zero, BigLong.context.zero)
     else UBigLongBigLongQuotientsByGCD(
@@ -80,7 +80,7 @@ internal value class BigLongUBigLongQuotientsByGCD(val first: BigLong, val secon
 }
 
 internal fun divideByGCD(first: BigLong, second: UBigLong): BigLongUBigLongQuotientsByGCD = context(UBigLong.context, BigLong.context) {
-    val gcd = gcd(first.absoluteValue, second)
+    val gcd = context(UBigLong.context) /* FIXME: KT-75150 */ { gcd(first.absoluteValue, second) }
     
     if (gcd.isZero()) BigLongUBigLongQuotientsByGCD(BigLong.context.zero, UBigLong.context.zero)
     else BigLongUBigLongQuotientsByGCD(
@@ -402,7 +402,7 @@ public data object BigLongRationalContext : Reification<BigLongRational>, Field<
             denominator = denominator,
         )
     override fun BigLongRational.plus(other: BigLongRational): BigLongRational = context(UBigLong.context, BigLong.context) {
-        val denominatorsGcd = gcd(denominator, other.denominator)
+        val denominatorsGcd = context(UBigLong.context) /* FIXME: KT-75150 */ { gcd(denominator, other.denominator) }
         val reducedThisDenominator = denominator / denominatorsGcd
         val reducedOtherDenominator = other.denominator / denominatorsGcd
         val numeratorCandidate = numerator * reducedOtherDenominator + reducedThisDenominator * other.numerator
@@ -413,7 +413,7 @@ public data object BigLongRationalContext : Reification<BigLongRational>, Field<
         )
     }
     override fun BigLongRational.minus(other: BigLongRational): BigLongRational = context(UBigLong.context, BigLong.context) {
-        val denominatorsGcd = gcd(denominator, other.denominator)
+        val denominatorsGcd = context(UBigLong.context) /* FIXME: KT-75150 */ { gcd(denominator, other.denominator) }
         val reducedThisDenominator = denominator / denominatorsGcd
         val reducedOtherDenominator = other.denominator / denominatorsGcd
         val numeratorCandidate = numerator * reducedOtherDenominator - reducedThisDenominator * other.numerator
