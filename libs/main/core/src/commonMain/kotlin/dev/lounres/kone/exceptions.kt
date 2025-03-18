9bf3833fe85adffd72e4d40f1ce30445734e42be
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-03-18 15:42:12 +0300

    Experiment in progress.

--- libs/main/core/src/commonMain/kotlin/dev/lounres/kone/exceptions.kt
+++ libs/main/core/src/commonMain/kotlin/dev/lounres/kone/exceptions.kt
@@ -10,7 +10,7 @@ package dev.lounres.kone
  * Exception that describes that some functions' (or properties') implementations are intentionally not implemented
  * but were called.
  *
- * Usually it is anti-pattern to intentionally not implement API parts.
+ * Usually it is antipattern to intentionally not implement API parts.
  * But it is useful for testing or inner implementations purposes.
  */
 public open class IllegalCallException(message: String) : RuntimeException(message)
