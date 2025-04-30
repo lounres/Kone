77dbed4e39773d2053b30c44f20c7fe18e661f66
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-04-30 21:47:59 +0300

    Experiment in progress.

--- libs/main/relations/benchmarks/build.gradle.kts
+++ libs/main/relations/benchmarks/build.gradle.kts
@@ -1,16 +1,21 @@
+plugins {
+    alias(versions.plugins.kotlinx.serialization)
+}
+
 kotlin {
     sourceSets {
         commonMain {
             dependencies {
                 implementation(projects.libs.main.algebraic)
+                implementation(versions.kotlinx.serialization.json)
             }
         }
     }
 }
 
-tasks.register<JavaExec>("jvmMainCustomBenchmark") {
-    group = "benchmark"
-    val jvmMainBenchmarkJar by tasks.getting
-    classpath = files(jvmMainBenchmarkJar)
-    args("--f=1", "--bm=avgt", "--wi=5", "--i=3", "--r=500ms", "--w=500ms", "--tu=ns", "--prof=gc", /*"--prof=xperfasm",*/ "--prof=stack")
-}
+//tasks.register<JavaExec>("jvmMainCustomBenchmark") {
+//    group = "benchmark"
+//    val jvmMainBenchmarkJar by tasks.getting
+//    classpath = files(jvmMainBenchmarkJar)
+//    args("--f=1", "--bm=avgt", "--wi=5", "--i=3", "--r=500ms", "--w=500ms", "--tu=ns", "--prof=gc", /*"--prof=xperfasm",*/ "--prof=stack")
+//}
