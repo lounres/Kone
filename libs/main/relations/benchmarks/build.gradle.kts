150076c05fa52651128e25282805ed4a2119ebf2
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-08-02 02:32:51 +0300

    Experiment in progress.

--- libs/main/relations/benchmarks/build.gradle.kts
+++ libs/main/relations/benchmarks/build.gradle.kts
@@ -16,5 +16,28 @@ kotlin {
 //    group = "benchmark"
 //    val jvmMainBenchmarkJar by tasks.getting
 //    classpath = files(jvmMainBenchmarkJar)
-//    args("--lprof", "--f=1", "--bm=avgt", "--wi=5", "--i=3", "--r=500ms", "--w=500ms", "--tu=ns", "--prof=gc", /*"--prof=",*/ /*"--prof=xperfasm",*/ "--prof=stack")
+//    val reportsDirectory =
+//    args(
+////        "--lprof",
+//        "--f=1",
+//        "--bm=avgt",
+//        "--wi=5",
+//        "--i=3",
+//        "--r=500ms",
+//        "--w=500ms",
+//        "--tu=ns",
+////        "--prof=async:dir=${project.layout.buildDirectory.asFile.get().resolve("reports")}",
+////        "--prof=cl",
+////        "--prof=comp",
+////        "--prof=gc",
+////        "--prof=jfr",
+////        "--prof=mempool",
+////        "--prof=pauses",
+////        "--prof=perf",
+////        "--prof=perfasm",
+////        "--prof=perfc2c",
+////        "--prof=perfnorm",
+////        "--prof=safepoints",
+////        "--prof=stack"
+//    )
 //}
