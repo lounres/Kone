bb96dca07b0e01d2afbce44a1947b7d36c1b047c
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2025-11-07 03:47:39 +0300

    Experiment in progress.

--- libs/main/concurrentCollections/src/commonMain/kotlin/dev/lounres/kone/concurrentCollections/KoneConcurrentSundellTsigasNoddedDequeueCleaned.kt
+++ libs/main/concurrentCollections/src/commonMain/kotlin/dev/lounres/kone/concurrentCollections/KoneConcurrentSundellTsigasNoddedDequeueCleaned.kt
@@ -5,10 +5,14 @@
 
 package dev.lounres.kone.concurrentCollections
 
+import kotlin.concurrent.atomics.AtomicInt
 import kotlin.concurrent.atomics.AtomicReference
+import kotlin.concurrent.atomics.decrementAndFetch
+import kotlin.concurrent.atomics.update
 
 
-public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
+// TODO: WIP
+public class KoneConcurrentSundellTsigasNoddedDequeueCleaned<Element> {
     internal val head = Node<Element>(null)
     internal val tail = Node<Element>(null)
     
@@ -54,44 +58,79 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
         @IgnorableReturnValue
         private fun <Element> correctPrev(prev: Node<Element>, node: Node<Element>): Node<Element> {
             var prev = prev
+            prev.reference()
             var lastLink: Node<Element>? = null
             while (true) {
                 val link = node.prev.load()!!
-                if (link.isBeingDeleted) break
+                link.node.reference()
+                if (link.isBeingDeleted) {
+                    link.node.dereference()
+                    break
+                }
                 val prev2 = prev.next.load()!!
+                prev2.node.reference()
                 if (prev2.isBeingDeleted) {
                     if (lastLink != null) {
                         prev.markPrevLink()
-                        lastLink.next.checkEqualityAndSet(prev, false, Node.Link(prev2.node, false))
+                        if (lastLink.next.checkEqualityAndSet(prev, false, Node.Link(prev2.node, false))) {
+                            prev2.node.reference()
+                            prev.dereference()
+                        }
+                        prev.dereference()
                         prev = lastLink
                         lastLink = null
+                        link.node.dereference()
+                        prev2.node.dereference()
                         continue
                     }
-                    prev = prev.prev.load()!!.node
+                    prev = prev.prev.load()!!.node.also { prev.dereference() }
+                    prev.reference()
+                    link.node.dereference()
+                    prev2.node.dereference()
                     continue
                 }
                 if (prev2.node !== node) {
+                    lastLink?.dereference()
                     lastLink = prev
                     prev = prev2.node
+                    link.node.dereference()
+                    prev2.node.dereference()
                     continue
                 }
+                prev2.node.dereference()
                 if (node.prev.compareAndSet(link, Node.Link(prev, false))) {
-                    if (prev.prev.load()?.isBeingDeleted == true) continue
+                    link.node.dereference()
+                    prev.reference()
+                    link.node.dereference()
+                    if (prev.prev.load()?.isBeingDeleted == true) {
+                        continue
+                    }
                     break
                 }
+                link.node.dereference()
                 // BACK-OFF
             }
+            prev.dereference()
+            lastLink?.dereference()
             return prev
         }
         
         private fun <Element> Node<Element>.pushEnd(next: Node<Element>) {
             while (true) {
                 val link = next.prev.load()!!
-                if (link.isBeingDeleted || this.next.load()!!.let { it.node !== next || it.isBeingDeleted }) break
+                link.node.reference()
+                if (link.isBeingDeleted || this.next.load()!!.let { it.node !== next || it.isBeingDeleted }) {
+                    link.node.dereference()
+                    break
+                }
                 if (next.prev.compareAndSet(link, Node.Link(this, false))) {
+                    link.node.dereference()
+                    this.reference()
+                    link.node.dereference()
                     if (this.prev.load()!!.isBeingDeleted) correctPrev(this, next)
                     break
                 }
+                link.node.dereference()
             }
         }
     }
@@ -102,12 +141,20 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
         val prev = head
         while (true) {
             val next = prev.next.load()!!.node
-            newNode.prev.store(Node.Link(prev, false))
-            newNode.next.store(Node.Link(next, false))
+            next.reference()
+            newNode.prev.exchange(Node.Link(prev, false))?.node?.dereference()
+            prev.reference()
+            newNode.next.exchange(Node.Link(next, false))?.node?.dereference()
+            next.reference()
             if (prev.next.checkEqualityAndSet(next, false, Node.Link(newNode, false))) {
+                next.dereference()
+                next.dereference()
+                newNode.reference()
                 newNode.pushEnd(next)
+                newNode.dereference()
                 return newNode
             }
+            next.dereference()
             // BACK-OFF
         }
     }
@@ -117,14 +164,24 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
         val newNode = Node(element)
         val next = tail
         var prev = next.prev.load()!!.node
+        prev.reference()
         while (true) {
-            newNode.prev.store(Node.Link(prev, false))
-            newNode.next.store(Node.Link(next, false))
-            if (prev.next.checkEqualityAndSet(next, false, Node.Link(newNode, false))) break
-            prev = correctPrev(prev, next)
+            newNode.prev.exchange(Node.Link(prev, false))?.node?.dereference()
+            prev.reference()
+            newNode.next.exchange(Node.Link(next, false))?.node?.dereference()
+            next.reference()
+            if (prev.next.checkEqualityAndSet(next, false, Node.Link(newNode, false))) {
+                next.dereference()
+                newNode.reference()
+                break
+            }
+            prev = correctPrev(prev, next).also { prev.dereference() }
+            prev.reference()
             // BACK-OFF
         }
+        prev.dereference()
         newNode.pushEnd(next)
+        newNode.dereference()
         return newNode
     }
     
@@ -133,17 +190,32 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
         val prev = head
         while (true) {
             val node = prev.next.load()!!.node
-            if (node === tail) return null
+            node.reference()
+            if (node === tail) {
+                node.dereference()
+                return null
+            }
             val next = node.next.load()!!
+            next.node.reference()
             if (next.isBeingDeleted) {
                 node.markPrevLink()
-                prev.next.checkNodeEqualityAndSet(node, Node.Link(next.node, false))
+                if (prev.next.checkNodeEqualityAndSet(node, Node.Link(next.node, false))) {
+                    next.node.reference()
+                    node.dereference()
+                }
+                node.dereference()
+                next.node.dereference()
                 continue
             }
             if (node.next.compareAndSet(next, Node.Link(next.node, true))) {
                 correctPrev(prev, next.node)
+                prev.dereference()
+                node.dereference()
+                next.node.dereference()
                 return node
             }
+            node.dereference()
+            next.node.dereference()
             // BACK-OFF
         }
     }
@@ -152,15 +224,23 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
     public fun popLastMaybe(): Node<Element>? {
         val next = tail
         var node = next.prev.load()!!.node
+        node.reference()
         while (true) {
             if (node.next.load()!!.let { it.node !== next || it.isBeingDeleted }) {
-                node = correctPrev(node, next)
+                node = correctPrev(node, next).also { node.dereference() }
+                node.reference()
                 continue
             }
-            if (node === head) return null
+            if (node === head) {
+                node.dereference()
+                return null
+            }
             if (node.next.checkEqualityAndSet(next, false, Node.Link(next, true))) {
                 val prev = node.prev.load()!!.node
+                prev.reference()
                 correctPrev(prev, next)
+                prev.dereference()
+                node.dereference()
                 return node
             }
             // BACK-OFF
@@ -179,8 +259,28 @@ public class KoneConcurrentSundellTsigasNoddedDequeue<Element> {
             val isBeingDeleted: Boolean = false,
         )
         
-        internal val prev: AtomicReference<Link<Element>?> = AtomicReference(null)
-        internal val next: AtomicReference<Link<Element>?> = AtomicReference(null)
+        private var _prev: AtomicReference<Link<Element>?>? = AtomicReference(null)
+        internal val prev: AtomicReference<Link<Element>?> get() = _prev ?: error("Accessing disposed previous atomic reference")
+        private var _next: AtomicReference<Link<Element>?>? = AtomicReference(null)
+        internal val next: AtomicReference<Link<Element>?> get() = _next ?: error("Accessing disposed next atomic reference")
+        
+        private var referenceCounter: AtomicInt? = AtomicInt(1)
+        
+        private fun dispose() {
+            next.exchange(null)?.node?.dereference()
+            _next = null
+            prev.exchange(null)?.node?.dereference()
+            _prev = null
+            referenceCounter = null
+        }
+        
+        internal fun reference() {
+            referenceCounter!!.update { it + 1 }
+        }
+        
+        internal fun dereference() {
+            if (referenceCounter!!.decrementAndFetch() == 0) dispose()
+        }
         
         public fun remove() {
             while (true) {
---
+++ libs/main/concurrentCollections/src/jvmTest/kotlin/dev/lounres/kone/concurrentCollections/KoneConcurrentSundellTsigasNoddedDequeueCleanedConcurrencyTest.kt
@@ -0,0 +1,182 @@
+/*
+ * Copyright © 2025 Gleb Minaev
+ * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
+ */
+
+package dev.lounres.kone.concurrentCollections
+
+import dev.lounres.kone.collections.deque.implementations.KoneListBackedDeque
+import dev.lounres.kone.collections.deque.isEmpty
+import dev.lounres.kone.collections.deque.isNotEmpty
+import dev.lounres.kone.collections.deque.popFirst
+import dev.lounres.kone.collections.deque.popLast
+import dev.lounres.kone.collections.iterables.isNotEmpty
+import dev.lounres.kone.collections.list.KoneMutableListNode
+import dev.lounres.kone.collections.list.implementations.KoneGCLinkedSizedList
+import dev.lounres.kone.collections.list.lastIndex
+import dev.lounres.kone.collections.utils.first
+import dev.lounres.kone.collections.utils.last
+import org.jetbrains.lincheck.datastructures.ModelCheckingOptions
+import org.jetbrains.lincheck.datastructures.Operation
+import org.jetbrains.lincheck.datastructures.StressOptions
+import kotlin.test.Test
+
+
+class KoneConcurrentSundellTsigasNoddedDequeueCleanedConcurrencyTest {
+    typealias Element = Int
+    
+    private val dequeue = KoneConcurrentSundellTsigasNoddedDequeueCleaned<Element>()
+    private val nodesDequeue = KoneListBackedDeque<KoneConcurrentSundellTsigasNoddedDequeueCleaned.Node<Element>>()
+    
+//    @StateRepresentation
+//    fun stateRepresentation() = buildString {
+//        appendLine()
+//        appendLine("  head: ${dequeue.head}")
+//        appendLine("  tail: ${dequeue.tail}")
+//
+//        scope {
+//            val nodes = mutableSetOf<KoneConcurrentSundellTsigasNoddedDequeueCleaned.Node<Element>>()
+//            val nodesToCheck = mutableSetOf(dequeue.head, dequeue.tail)
+//            while (nodesToCheck.isNotEmpty()) {
+//                val currentNode = nodesToCheck.first()
+//                nodesToCheck.remove(currentNode)
+//                nodes.add(currentNode)
+//                val newNodes = setOfNotNull(currentNode.prev.load()?.node, currentNode.next.load()?.node)
+//                for (newNode in newNodes) if (newNode !in nodes) nodesToCheck.add(newNode)
+//            }
+//            appendLine("  reachable nodes:")
+//            for (node in nodes) {
+//                appendLine("    $node:")
+//
+//                appendLine("      next:")
+//                val nextLink = node.next.load()
+//                if (nextLink != null) {
+//                    appendLine("        node: ${nextLink.node}")
+//                    appendLine("        isBeingDeleted: ${nextLink.isBeingDeleted}")
+//                } else {
+//                    appendLine("        null")
+//                }
+//
+//                appendLine("      prev:")
+//                val prevLink = node.prev.load()
+//                if (prevLink != null) {
+//                    appendLine("        node: ${prevLink.node}")
+//                    appendLine("        isBeingDeleted: ${prevLink.isBeingDeleted}")
+//                } else {
+//                    appendLine("        null")
+//                }
+//            }
+//        }
+//    }
+    
+    @Operation
+    fun addFirst(element: Element) {
+        dequeue.addFirst(element)
+    }
+    
+    @Operation
+    fun addLast(element: Element) {
+        dequeue.addLast(element)
+    }
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun addFirstPushFirst(element: Element) {
+        nodesDequeue.addFirst(dequeue.addFirst(element))
+    }
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun addFirstPushLast(element: Element) {
+        nodesDequeue.addLast(dequeue.addFirst(element))
+    }
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun addLastPushFirst(element: Element) {
+        nodesDequeue.addFirst(dequeue.addLast(element))
+    }
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun addLastPushLast(element: Element) {
+        nodesDequeue.addLast(dequeue.addLast(element))
+    }
+
+    @Operation
+    fun popFirstMaybe() = dequeue.popFirstMaybe()?.value
+    
+    @Operation
+    fun popLastMaybe() = dequeue.popLastMaybe()?.value
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun removeFirstNode() = nodesDequeue.run { if (isEmpty()) null else popFirst() }?.also { it.remove() }?.value
+    
+    @Operation(nonParallelGroup = "nodesDequeue")
+    fun removeLastNode() = nodesDequeue.run { if (isEmpty()) null else popLast() }?.also { it.remove() }?.value
+    
+    class SequentialSpecification {
+        private val dequeue = KoneGCLinkedSizedList<Element>()
+        private val nodesDequeue = KoneListBackedDeque<KoneMutableListNode<Element>>()
+        
+        @Operation
+        fun addFirst(element: Element) {
+            dequeue.addAt(0u, element)
+        }
+        
+        @Operation
+        fun addLast(element: Element) {
+            dequeue.add(element)
+        }
+
+        @Operation
+        fun addFirstPushFirst(element: Element) {
+            nodesDequeue.addFirst(dequeue.addNodeAt(0u, element))
+        }
+
+        @Operation
+        fun addFirstPushLast(element: Element) {
+            nodesDequeue.addLast(dequeue.addNodeAt(0u, element))
+        }
+
+        @Operation
+        fun addLastPushFirst(element: Element) {
+            nodesDequeue.addFirst(dequeue.addNode(element))
+        }
+
+        @Operation
+        fun addLastPushLast(element: Element) {
+            nodesDequeue.addLast(dequeue.addNode(element))
+        }
+
+        @Operation
+        fun popFirstMaybe() =
+            if (dequeue.isNotEmpty()) dequeue.first().also { dequeue.removeAt(0u) }
+            else null
+
+        @Operation
+        fun popLastMaybe() =
+            if (dequeue.isNotEmpty()) dequeue.last().also { dequeue.removeAt(dequeue.lastIndex) }
+            else null
+
+        @Operation
+        fun removeFirstNode() =
+            if (nodesDequeue.isNotEmpty()) nodesDequeue.popFirst().also { if (!it.isDetached) it.remove() }.element
+            else null
+
+        @Operation
+        fun removeLastNode() =
+            if (nodesDequeue.isNotEmpty()) nodesDequeue.popLast().also { if (!it.isDetached) it.remove() }.element
+            else null
+    }
+    
+    @Test
+    fun stress() {
+        StressOptions()
+            .sequentialSpecification(SequentialSpecification::class.java)
+            .check(this::class)
+    }
+    
+    @Test
+    fun modelChecking() {
+        ModelCheckingOptions()
+            .sequentialSpecification(SequentialSpecification::class.java)
+            .check(this::class)
+    }
+}
