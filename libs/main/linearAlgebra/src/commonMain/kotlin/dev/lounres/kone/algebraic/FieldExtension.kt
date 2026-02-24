475725eae9afc7c5111f3955e99a5ef368e26e1d
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2026-02-25 00:38:29 +0300

    Experiment in progress.

---
+++ libs/main/linearAlgebra/src/commonMain/kotlin/dev/lounres/kone/algebraic/FieldExtension.kt
@@ -0,0 +1,62 @@
+package dev.lounres.kone.algebraic
+
+import dev.lounres.kone.registry.ImpliedKeysRegistry
+import dev.lounres.kone.registry.RegistryKey
+import dev.lounres.kone.suppliedTypes.DelicateSuppliedTypeConstructor
+import dev.lounres.kone.suppliedTypes.SuppliedProjection
+import dev.lounres.kone.suppliedTypes.SuppliedType
+import kotlin.jvm.JvmName
+import kotlin.reflect.KVariance.INVARIANT
+
+
+@Suppress("INAPPLICABLE_JVM_NAME")
+public interface FieldExtension<Number, Vector> : CommutativeAlgebra<Number, Vector>, Field<Vector> {
+    // region Vector-Number operations
+    @JvmName("divVectorNumber")
+    public operator fun Vector.div(other: Number): Vector = this / valueOf(other)
+    // endregion
+    
+    // region Number-Vector operations
+    @JvmName("divNumberVector")
+    public operator fun Number.div(other: Vector): Vector = valueOf(this) / other
+    // endregion
+    
+    public companion object;
+    
+    public class Key<Number, Vector>(
+        public val numberType: SuppliedType,
+        public val vectorType: SuppliedType,
+    ) : RegistryKey<FieldExtension<Number, Vector>> {
+        public val typeKey: SuppliedType.Regular =
+            @OptIn(DelicateSuppliedTypeConstructor::class)
+            SuppliedType.Regular(
+                fullyQualifiedName = "dev.lounres.kone.algebraic.FieldExtension",
+                typeArguments = listOf(
+                    SuppliedProjection.Regular(
+                        variance = INVARIANT,
+                        type = numberType
+                    ),
+                    SuppliedProjection.Regular(
+                        variance = INVARIANT,
+                        type = vectorType
+                    ),
+                ),
+                isNullable = false
+            )
+        override val impliedKeys: ImpliedKeysRegistry<FieldExtension<Number, Vector>> = ImpliedKeysRegistry {
+            Algebra.Key<Number, Vector>(numberType, vectorType) implies { it }
+            Field.Key<Vector>(vectorType) implies { it }
+        }
+        override fun equals(other: Any?): Boolean = other is Key<*, *> && typeKey == other.typeKey
+        override fun hashCode(): Int = typeKey.hashCode()
+        override fun toString(): String = "dev.lounres.kone.algebraic.FieldExtension.Key<$numberType, $vectorType>"
+    }
+}
+
+@JvmName("divVectorNumber")
+context(fieldExtension: FieldExtension<Number, Vector>)
+public operator fun <Number, Vector> Vector.div(other: Number): Vector = with(fieldExtension) { this@div / other }
+
+@JvmName("divNumberVector")
+context(fieldExtension: FieldExtension<Number, Vector>)
+public operator fun <Number, Vector> Number.div(other: Vector): Vector = with(fieldExtension) { this@div / other }
