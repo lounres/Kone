5da11f475bc58a48949b033767c76e4e39918550
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2026-03-31 23:26:19 +0300

    Experiment in progress.

--- libs/main/matrices/src/commonMain/kotlin/dev/lounres/kone/algebraic/MatrixFactory.declarations.kt
+++ libs/main/matrices/src/commonMain/kotlin/dev/lounres/kone/algebraic/MatrixFactory.declarations.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2026 Gleb Minaev
+ * All rights reserved. Licensed under the Apache License, Version 2.0. See the license in file LICENSE
+ */
+
 package dev.lounres.kone.algebraic
 
 import dev.lounres.kone.collections.map.KoneMap
