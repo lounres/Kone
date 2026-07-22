87e88e5f9009c0617015562d727b7a4baf6e7437
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2026-07-22 05:05:26 +0300

    Experiment in progress.

--- plugins/contexts/src/test/data/ir/test/test.kt
+++ plugins/contexts/src/test/data/ir/test/test.kt
@@ -5,39 +5,15 @@ package foo.bar
 import dev.lounres.kone.contexts.*
 
 
-fun interface Plus<in Left, in Right, out Result> : KoneContext {
-    operator fun Left.plus(other: Right): Result
+class Foo : KoneContext {
+    val bar = 57
 }
 
-fun interface Minus<in Left, in Right, out Result> : KoneContext {
-    public operator fun Left.minus(other: Right): Result
+context(foo: Foo)
+val baz: Int get() = foo.bar
+
+fun box(): String {
+    contexts(Foo(), 1, "2", 3.0) {
+        return if (baz == 57) "OK" else "INCORRECT"
+    }
 }
-
-interface Semigroup<Number> : KoneContext {
-    @KoneContextInclude
-    val numberPlusNumber: Plus<Number, Number, Number>
-}
-
-interface Group<Number> : Semigroup<Number> {
-    @KoneContextInclude
-    val numberMinusNumber: Minus<Number, Number, Number>
-}
-
-interface Semiring<Number> : Semigroup<Number> {
-    val one: Number
-}
-
-interface Ring<Number> : Semiring<Number>, Group<Number> {
-    @KoneContextInclude
-    public val numberPlusLong: Plus<Number, Long, Number>
-}
-
-public inline fun <Number> rightAddMultipliedByDoubling(base: Number, arg: Number, multiplier: Int, additionOp: (Number, Number) -> Number, rightSubtractionOp: (Number, Number) -> Number): Number = TODO()
-
-context(ring: Ring<Number>)
-public infix fun <Number> Number.doublingPlus(other: Int): Number {
-    KoneContext.localUnwrap(ring)
-    return rightAddMultipliedByDoubling<Number>(this@doublingPlus, ring.one, other, { left, right -> left + right }, { left, right -> left - right })
-}
-
-fun box(): String = "OK"
