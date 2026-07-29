966c82a6a22aef5e97fe4f69119f8d3b8f1d7b7f
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2026-07-30 00:20:24 +0300

    Experiment in progress.

--- libs/main/algebraic/src/commonMain/kotlin/dev/lounres/kone/algebraic/Semigroup.kt
+++ libs/main/algebraic/src/commonMain/kotlin/dev/lounres/kone/algebraic/Semigroup.kt
@@ -8,20 +8,36 @@ package dev.lounres.kone.algebraic
 import dev.lounres.kone.contexts.KoneContext
 import dev.lounres.kone.contexts.KoneContextInclude
 import dev.lounres.kone.registry.ImpliedKeysRegistry
+import dev.lounres.kone.registry.Registry
 import dev.lounres.kone.registry.SuppliedTypeRegistryKey
 import dev.lounres.kone.suppliedTypes.Suppliable
 import dev.lounres.kone.suppliedTypes.Supply
 import dev.lounres.kone.suppliedTypes.suppliedTypeOf
 
 
+/**
+ * Describes a context that represents [mathematical semigroup](https://en.wikipedia.org/wiki/Semigroup).
+ *
+ * @param Number The type of elements of the semigroup.
+ */
 public interface Semigroup<Number> : KoneContext {
     // region Number-Number operations
+    /**
+     * The associative binary addition operation on elements of type [Number].
+     *
+     * @return The addition context represented as [Plus] instance.
+     */
     @KoneContextInclude
     public val numberPlusNumber: Plus<Number, Number, Number>
     // endregion
     
     public companion object;
     
+    /**
+     * Registry key for [Semigroup] interface in [Registry].
+     *
+     * @param Number The type of elements of the semigroup.
+     */
     @Suppliable
     public class Key<@Supply Number> : SuppliedTypeRegistryKey<Semigroup<Number>>() {
         override val impliedKeys: ImpliedKeysRegistry<Semigroup<Number>> by lazy {
@@ -33,9 +49,19 @@ public interface Semigroup<Number> : KoneContext {
     }
 }
 
+/**
+ * Describes a context that represents [mathematical commutative semigroup](https://en.wikipedia.org/wiki/Semigroup) (a.k.a. abelian semigroup).
+ *
+ * @param Number The type of elements of the commutative semigroup.
+ */
 public interface CommutativeSemigroup<Number> : Semigroup<Number> {
     public companion object;
     
+    /**
+     * Registry key for [CommutativeSemigroup] interface in [Registry].
+     *
+     * @param Number The type of elements of the commutative semigroup.
+     */
     @Suppliable
     public class Key<@Supply Number> : SuppliedTypeRegistryKey<CommutativeSemigroup<Number>>() {
         override val impliedKeys: ImpliedKeysRegistry<CommutativeSemigroup<Number>> by lazy {
